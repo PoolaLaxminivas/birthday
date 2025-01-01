b7f7d9e66033915ce88c4223ce6784fba4a61915
PoolaLaxminivas <146500130+PoolaLaxminivas@users.noreply.github.com>
2025-01-01 22:28:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('giftBox').addEventListener('click', function() {
+  document.getElementById('cakePopup').style.display = 'block';
+});
+
+document.getElementById('closePopup').addEventListener('click', function() {
+  document.getElementById('cakePopup').style.display = 'none';
+});
